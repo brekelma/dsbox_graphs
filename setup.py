26acee79ec8903571975920911c106604fc89a97
Robert Brekelmans <brekelma@dsbox01.isi.edu>
2020-06-05 22:04:49 -0700

    a

--- setup.py
+++ setup.py
@@ -24,7 +24,7 @@ setup(
     ], 
     entry_points = {
     'd3m.primitives': [
-        'feature_construction.sdne.DSBOX = dsbox_graphs.sdne:SDNE',
+        #'feature_construction.sdne.DSBOX = dsbox_graphs.sdne:SDNE',
         'feature_construction.gcn_mixhop.DSBOX = dsbox_graphs.gcn_mixhop:GCN'
         #'data_transformation.graph_to_edge_list.DSBOX = graph_dataset_to_list:GraphDatasetToList',
         #'feature_construction.graph_transformer.SDNE = sdne:SDNE',
